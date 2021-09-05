adc6e316a9385d8a72b9adc09b71355d54100021
Vamsi Charan Adari <adarivamsi@gmail.com>
2021-09-05 11:02:27 -0400

    Added Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="app">
       <h1>Let's Build Tinder</h1>
+      {/* Header */}
+      {/* Tinder Cards */}
+      {/* Hand Swipe Options */}
     </div>
   );
 }
